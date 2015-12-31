f31b59368b5fd8a201a9faea9d1ab4681618a902
david <david@ongoingworlds.com>
2015-12-31 11:52:28 +0000

    Still working on character select

--- Scripts/Menu/PlayerSelectScript.js
+++ Scripts/Menu/PlayerSelectScript.js
@@ -1,13 +1,11 @@
-﻿#pragma strict
+#pragma strict
 
 static var playerSelection : Hashtable; //--chosen characters
-static var characters : Hashtable;		//--character refs
 
 //--create array of the possible different player characters 
 public var playerCharacters = new Array ("A", "B");
 
 private var numPlayers : int = 2;
-
 
 function Start () {
 
@@ -15,23 +13,19 @@ function Start () {
 	playerSelection = new Hashtable();
 	playerSelection.Add(1,"B");
 	playerSelection.Add(2,"B");
-	
-	//--hide all the characters, then show only the 1st
+		
+	//--hide all the characters apart from the 1st
 	for(var i : int = 1; i <= numPlayers; i++){
 	
-
-		for(var j : int = 0; j < playerCharacters.length; j++){
+		for(var j : int = 1; j < playerCharacters.length; j++){
 			Debug.Log("hide "+i+playerCharacters[j]);
 			
 			GameObject.Find("Player"+i+playerCharacters[j]).SetActive(false);
-		}	
-		
-		//Debug.Log("show"+i+playerCharacters[0]);
-		//GameObject.Find("Player"+i+playerCharacters[0]).SetActive(true);
-		
+		}			
 	}
-	GameObject.Find("Player1B").SetActive(false);
-	GameObject.Find("Player1B").SetActive(true);
+	
+//	GameObject.Find("Player1A").SetActive(true);
+//	GameObject.Find("Player2A").SetActive(true);
 	
 	//--load the main level
 //	Application.LoadLevel ("main");
